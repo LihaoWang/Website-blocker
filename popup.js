746d21da1bd31701220e2035981c40b4bbec67fe
leowang <leowang9863@gmail.com>
2021-03-20 13:42:50 -0400

    minor changes

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
-var toggle = document.getElementById("toggle");
-function updateText() {
+const toggle = document.getElementById("toggle");
+const updateText = () => {
   chrome.storage.local.get(["toggle"], function (result) {
     if (result.toggle) {
       toggle.innerText = "ON";
@@ -7,23 +7,15 @@ function updateText() {
       toggle.innerText = "OFF";
     }
   });
-}
+};
 updateText();
-chrome.storage.local.onChanged.addListener(updateText);
-// chrome.storage.local.get(["toggle"], function (result) {
-//   toggle.innerText = result.toggle;
-// });
 
-toggle.onclick = function () {
+chrome.storage.local.onChanged.addListener(updateText);
+
+toggle.onclick = () => {
   chrome.storage.local.get(["toggle"], function (result) {
     chrome.storage.local.set({ toggle: !result.toggle });
   });
 
   chrome.extension.getBackgroundPage().console.log("clicked");
 };
-
-// toggle.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   chrome.storage.local.set({ toggle: false });
-//   chrome.extension.getBackgroundPage().console.log("done");
-// });
